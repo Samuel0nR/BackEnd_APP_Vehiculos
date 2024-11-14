78677117ebac9922811b70b41d683985ddc6430f
Samuel0nR <sam.uel20@outlook.com>
2024-11-14 20:49:45 -0300

    Proyect Start - VehiclesAPI

---
+++ Models/VehiculoModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api_dotNet_vehicles.Models;
+
+public partial class VehiculoModel
+{
+    public int Id { get; set; }
+
+    public int CodVehi { get; set; }
+
+    public string Marca { get; set; } = null!;
+
+    public string Modelo { get; set; } = null!;
+
+    public string CodTipo { get; set; } = null!;
+
+    public int? Velocidades { get; set; }
+
+    public string? Suspension { get; set; }
+
+    public string? Frenos { get; set; }
+
+    public string? Cuadro { get; set; }
+
+    public string? Llanta { get; set; }
+
+    public string? Valvula { get; set; }
+
+    public string? Peso { get; set; }
+}
