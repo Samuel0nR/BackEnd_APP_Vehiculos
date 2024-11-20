3ff9757cf84f0fe9b120087c9dfa2f0b28240877
SamuelR <sam.uel20@outlook.com>
2024-11-20 01:31:50 -0300

    Add new fix prebuild

--- bin/prebuild.sh
+++ bin/prebuild.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "Ejecutando script prebuild"
+
 # Verificar si el directorio .dotnet ya existe
 if [ -d "/app/.dotnet" ]; then
+  echo "Eliminando directorio /app/.dotnet"
   # Eliminar el directorio .dotnet
   rm -rf /app/.dotnet
+else
+  echo "El directorio .dotnet no existe"
 fi
