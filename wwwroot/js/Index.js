49ab205b0017ec51b2449badfe54f8fc4124f214
Samuel0nR <sam.uel20@outlook.com>
2025-01-09 00:10:47 -0300

    Add news Fixes to Sidebar and add new View APIEndp

--- wwwroot/js/Index.js
+++ wwwroot/js/Index.js
@@ -3,6 +3,16 @@ $('#swaggerBtn').click(function () {
     window.location.href = '/swagger';
 });
 
+$('#menu-homeTab').click(function () {
+    $('#dropdownMenu').hide();
+});
 $('#menu-docTab').click(function () {
     $('#dropdownMenu').toggle();
 });
+
+$('#menu-apiEndp').click(function () {
+    $('#menu-docTab, #tabDoc').removeClass('active')
+    //$('#tabDoc').removeClass('show, active')
+
+});
+
